b044270ebf9c436fda34056a6ef480ef060d4a3d
Larry Pham <larrypham1991@yahoo.com>
2015-03-31 16:15:48 -0500

    added new files

---
+++ TestSite/java.js
@@ -0,0 +1,7 @@
+function myFunction() {
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+console.log(username);
+console.log(password);
+alert(username+"\n"+password);
+}
