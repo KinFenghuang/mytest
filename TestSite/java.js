f0c13e3beffdfe05ccb874edaccf2910f1e6c662
Larry Pham <larrypham1991@yahoo.com>
2015-03-31 16:26:39 -0500

    removing files

--- TestSite/java.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-console.log(username);
-console.log(password);
-alert(username+"\n"+password);
-}
